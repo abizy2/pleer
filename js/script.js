709991b29916221ad7a80c5c9cede0fd657b9f94
abizy2 <34310497+abizy2@users.noreply.github.com>
2020-04-17 03:42:41 +0300

    edit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 var video;
 
+
 window.onload = function() {
 	video = document.getElementById('movie');
 };
@@ -15,4 +16,17 @@ function pause() {
 function stop() {
     video.pause();
     video.currentTime = 0;
+}
+
+$(document).ready(function(){
+  $("#movie").on(
+    "timeupdate", 
+    function(event){
+      onTrackedVideoFrame(this.currentTime, this.duration);
+    });
+});
+
+function onTrackedVideoFrame(currentTime, duration){
+    $("#current").text(currentTime); 
+    $("#duration").text(duration)
 }
